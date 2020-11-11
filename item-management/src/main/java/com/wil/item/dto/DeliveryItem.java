cc265abfc663746d1e778e364d2f4d27d0ef1469
WilC96 <47301499+WilC96@users.noreply.github.com>
2020-11-11 21:35:15 +0000

    Update DeliveryItem.java

--- item-management/src/main/java/com/wil/item/dto/DeliveryItem.java
+++ item-management/src/main/java/com/wil/item/dto/DeliveryItem.java
@@ -2,8 +2,6 @@ package com.wil.item.dto;
 
 public interface DeliveryItem {
 	
-	//Have get before each property name
-	//So Spring Data knows that this data transfer object is used to populate the table with the data coming from the query
 	public String getDeliveryId();
 	public String getCompanyName();
 	public int getItemCount();
