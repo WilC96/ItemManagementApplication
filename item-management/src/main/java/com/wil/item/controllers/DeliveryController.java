8a1c3e686ae91a24cdf3620b5d0a6e01f3c92b8a
WilC96 <47301499+WilC96@users.noreply.github.com>
2020-11-11 21:33:30 +0000

    Update DeliveryController.java

--- item-management/src/main/java/com/wil/item/controllers/DeliveryController.java
+++ item-management/src/main/java/com/wil/item/controllers/DeliveryController.java
@@ -39,21 +39,20 @@ public class DeliveryController {
 	@GetMapping("/new")
 	public String createItemForm(Model model) {
 		
-		Delivery delivery = new Delivery(); //empty object to map to the form submission
-		model.addAttribute("delivery", delivery); //binds empty object to name specified in th:object 
+		Delivery delivery = new Delivery();
+		model.addAttribute("delivery", delivery); 
 		
+		//only return items that aren't already in a delivery
 		List<Item> items = itemRepository.findAll();
 		List<Item> itemsWithNull = items.stream().filter(i -> i.getDelivery() == null).collect(Collectors.toList());
-		//^Only return items that aren't already in a delivery. Without this, the new delivery will just overwrite the first
-		model.addAttribute("allItems", itemsWithNull); //carries over the list of items from db for the html when selecting items dropdown
+		
+		model.addAttribute("allItems", itemsWithNull);
 		
 		return "deliveries/new-delivery"; 
-		//name of html page(in template) for reroute on url /items/new, .html not required
 	}
 	
-	@PostMapping("/save") // method is POST in the template
+	@PostMapping("/save")
 	public String createDelivery(Delivery delivery, @RequestParam List<Long> items, Model model) {
-		//RequestParam name(items) needs to match the request param (object(entity) property name)
 		
 		deliveryRepository.save(delivery);
 		
@@ -61,13 +60,11 @@ public class DeliveryController {
 		Iterable<Item> chosenItems = itemRepository.findAllById(items);
 		
 		//For each item, update its delivery field (so the foreign key is not null) and save into the db
-		//It is necessary to update the items from DeliveryController as delivery is assigned to items table when delivery is created
 		for (Item item : chosenItems) {
 			item.setDelivery(delivery);
 			itemRepository.save(item);
 		}
 		
-		//prevent duplicate submissions(spamming submit) via redirect to any page
 		return "redirect:/delivery/new";
 	}
 	
@@ -77,10 +74,11 @@ public class DeliveryController {
 		Delivery delivery = deliveryRepository.findByDeliveryId(deliveryId);
 		model.addAttribute("delivery", delivery);
 		
+		//only return items that aren't already in a delivery
 		List<Item> items = itemRepository.findAll();
 		List<Item> itemsWithNull = items.stream().filter(i -> i.getDelivery() == null).collect(Collectors.toList());
-		//^Only return items that aren't already in a delivery. Without this, the new delivery will just overwrite the first
-		model.addAttribute("allItems", itemsWithNull); //carries over the list of items from db for the html when selecting items dropdown
+		
+		model.addAttribute("allItems", itemsWithNull);
 		
 		return "deliveries/new-delivery";
 	}
